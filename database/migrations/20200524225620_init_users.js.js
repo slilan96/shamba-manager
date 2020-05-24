f45f030ea6ff4defbe01c89b24a833204b7e498e
slilan <slilan96@gmail.com>
2020-05-24 23:08:29 +0300

    create user table

---
+++ database/migrations/20200524225620_init_users.js.js
@@ -0,0 +1,16 @@
+// create users table
+exports.up = function(knex) {
+  return knex.schema.createTable('users', (table) => {
+    table.increments('id').primary();
+    table.string('email');
+    table.string('password');
+    table.string('first_name');
+    table.string('last_name');
+    table.string('role');
+    table.timestamps(false, true);
+  });
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTable('users');
+};
--- src/models/users.model.js
+++ src/models/users.model.js
@@ -12,13 +12,10 @@ class users extends Model {
     return {
       type: 'object',
       required: ['password'],
-
       properties: {
-      
         email: { type: ['string', 'null'] },
         password: 'string',
-      
-      }
+      },
     };
   }
 
@@ -31,26 +28,7 @@ class users extends Model {
   }
 }
 
+//eslint-disable-next-line
 module.exports = function (app) {
-  const db = app.get('knex');
-
-  db.schema.hasTable('users').then(exists => {
-    if (!exists) {
-      db.schema.createTable('users', table => {
-        table.increments('id');
-      
-        table.string('email').unique();
-        table.string('password');
-      
-      
-        table.timestamp('createdAt');
-        table.timestamp('updatedAt');
-      })
-        .then(() => console.log('Created users table')) // eslint-disable-line no-console
-        .catch(e => console.error('Error creating users table', e)); // eslint-disable-line no-console
-    }
-  })
-    .catch(e => console.error('Error creating users table', e)); // eslint-disable-line no-console
-
   return users;
 };
