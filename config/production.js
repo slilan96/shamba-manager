8965bc30392d03cabbd6e2f09feb7db35218a03a
slilan <slilan96@gmail.com>
2020-07-11 17:29:55 +0300

    Add more config options to production config

--- config/production.js
+++ config/production.js
@@ -1,4 +1,29 @@
 module.exports = {
+  authentication: {
+    entity: 'user',
+    service: 'users',
+    secret: process.env.JWT_SECRET,
+    authStrategies: [
+      'jwt',
+      'local'
+    ],
+    jwtOptions: {
+      header: {
+        'typ': 'access'
+      },
+      audience: process.env.JWT_AUDIENCE,
+      issuer: 'feathers',
+      algorithm: 'HS256',
+      expiresIn: '1d'
+    },
+    local: {
+      usernameField: 'email',
+      passwordField: 'password'
+    }
+  },
+
+  host: 3030,
+  public: '../public',
   postgres: {
     client: 'pg',
     connection: process.env.DATABASE_URL,
