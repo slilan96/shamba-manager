ba1b3d63daeea5480231484ac73bdfcd0bf0e200
slilan <slilan96@gmail.com>
2020-07-22 20:52:44 +0300

    style: fix lint error in src/objection.js file

--- src/objection.js
+++ src/objection.js
@@ -1,6 +1,6 @@
 const { Model } = require('objection');
 
-module.exports = function (app) {
+module.exports = function initializeObjection(app) {
   const { client, connection } = app.get('postgres');
   // eslint-disable-next-line global-require
   const knex = require('knex')({ client, connection, useNullAsDefault: false });
